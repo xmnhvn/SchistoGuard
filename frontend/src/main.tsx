31de2273f3a1b8c2c1006ba6d71e84ced7f4086c
xmnhvn <xneamanlangit1216@gmail.com>
2026-04-12 20:15:53 +0800

    Register service worker for PWA support and enable offline readiness

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,17 @@
 import { createRoot } from "react-dom/client";
-  import App from "./App.tsx";
-  import "./index.css";
-  import 'leaflet/dist/leaflet.css';
+import { registerSW } from 'virtual:pwa-register';
+import App from "./App.tsx";
+import "./index.css";
+import 'leaflet/dist/leaflet.css';
+
+registerSW({
+  immediate: true,
+  onNeedRefresh() {
+    window.location.reload();
+  },
+  onOfflineReady() {
+    console.log('SchistoGuard PWA is ready for offline use');
+  },
+});
 
   createRoot(document.getElementById("root")!).render(<App />);
